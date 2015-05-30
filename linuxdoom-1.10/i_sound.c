8b96b066da2f836b44d8608f2535c6ebb14883d1
Adar Arnon <adararnon@gmail.com>
2015-05-30 20:09:36 +0300

    Fixed compilation errors in linuxdoom

--- linuxdoom-1.10/i_sound.c
+++ linuxdoom-1.10/i_sound.c
@@ -30,6 +30,8 @@ rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 
 #include <math.h>
 
+#include <errno.h>
+
 #include <sys/time.h>
 #include <sys/types.h>
 
@@ -163,7 +165,6 @@ myioctl
   int*	arg )
 {   
     int		rc;
-    extern int	errno;
     
     rc = ioctl(fd, command, arg);  
     if (rc < 0)
